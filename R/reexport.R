1d1e5f33bfddececf9d530c61fdbca8b3a6e44a2
Dan Chaltiel <dan.chaltiel@gmail.com>
2020-04-29 15:19:40 +0200

    reexport pipe and tidyselect helpers

---
+++ R/reexport.R
@@ -0,0 +1,32 @@
+
+#' @importFrom magrittr %>%
+#' @export
+magrittr::`%>%`
+
+#' @importFrom tidyselect everything
+#' @export
+tidyselect::everything
+
+#' @importFrom tidyselect all_of
+#' @export
+tidyselect::all_of
+
+#' @importFrom tidyselect any_of
+#' @export
+tidyselect::any_of
+
+#' @importFrom tidyselect starts_with
+#' @export
+tidyselect::starts_with
+
+#' @importFrom tidyselect ends_with
+#' @export
+tidyselect::ends_with
+
+#' @importFrom tidyselect contains
+#' @export
+tidyselect::contains
+
+#' @importFrom tidyselect matches
+#' @export
+tidyselect::matches
