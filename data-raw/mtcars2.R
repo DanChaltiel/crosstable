ddfa35989db3ccb58f4f5b5232e0783e1dda458d
Dan Chaltiel <dan.chaltiel@gmail.com>
2020-07-10 18:54:49 +0200

    added date and datetime to mtcars2

--- data-raw/mtcars2.R
+++ data-raw/mtcars2.R
@@ -17,6 +17,10 @@ mtcars2 = mtcars %>%
         gear="Number of forward gears",
         carb="Number of carburetors"
     ) %>% 
-    as_tibble()
+    mutate(
+        hp_date = as.Date(hp , origin="2010-01-01") %>% set_label("Some nonsense date"),
+        qsec_posix = as.POSIXct(qsec*3600*24 , origin="2010-01-01") %>% set_label("Date+time")
+    )
+as_tibble()
 
 usethis::use_data(mtcars2, overwrite=TRUE)
