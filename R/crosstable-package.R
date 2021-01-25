13e5ab16a1264f21c6002d2f4c891a6c3773c65d
Dan Chaltiel <dan.chaltiel@gmail.com>
2021-01-25 19:55:18 +0100

    update documentation

--- R/crosstable-package.R
+++ R/crosstable-package.R
@@ -5,3 +5,34 @@
 #' @name crosstable-package
 #' @aliases Test-package
 "_PACKAGE"
+
+
+
+
+
+#' Options for the package `crosstable` 
+#' @name crosstable_options
+#'
+#' @description 
+#' Here is a comprehensive list of all options that you can set globally.
+#' 
+#' For flextables:
+#'  + `crosstable_autofit`:
+#'  + `crosstable_compact`:
+#'  + `crosstable_show_test_name`:
+#'  + `crosstable_compact_padding`:
+#'  + `crosstable_wrap_id`: if id contains no spaces, wrap it with this maximum number of characters 
+#'  
+#' For rounding: 
+#'  + `crosstable_only_round`:
+#'  
+#' For specifying styles in officer docx output: 
+#'  + `crosstable_style_normal`:
+#'  + `crosstable_style_heading`:
+#'  + `crosstable_style_strong`:
+#'  + `crosstable_style_image`:
+#'  + `crosstable_style_legend`:
+#'  + `crosstable_style_list_ordered`:
+#'  + `crosstable_style_list_unordered`:
+#' 
+NULL
