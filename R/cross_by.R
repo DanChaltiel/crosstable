684c99a0ea2a624be0de2d805c659829b75be3ef
Dan Chaltiel <dan.chaltiel@gmail.com>
2021-01-31 21:01:07 +0100

    Check that column does not have both NA and `NA` *
    Also changed error gathering system from dataframe to environment

--- R/cross_by.R
+++ R/cross_by.R
@@ -1,5 +1,5 @@
 
-#' @importFrom purrr map_dfr
+#' @importFrom purrr imap_dfr map_dfr
 #' @importFrom survival is.Surv
 #' @importFrom glue glue glue_data glue_collapse
 #' @keywords internal
@@ -7,29 +7,39 @@
 cross_by = function(data_x, data_y, funs, funs_arg, margin, total, percent_digits, 
                     showNA, label, test, times, followup, 
                     test_args, cor_method, effect, effect_args){
+    errors = rlang::env()
     
-    funs = clear_funs(funs)
-    errors = data.frame(name=character(0), class=character(0))
-    
-    rtn_tbl = map_dfr(names(data_x), ~{
-        if(all(is.na(data_x[[.x]]))) data_x[[.x]] = "NA"
-        
-        if(inherits(data_x[[.x]], "difftime")){ 
-            lab=get_label(data_x[[.x]])
-            data_x[[.x]] =  as.numeric(data_x[[.x]]) %>% set_label(lab)
+    rtn_tbl = imap_dfr(data_x, ~{
+        if(all(is.na(.x))) .x = "NA"
+        if(anyNA(.x) && "NA" %in% .x) {
+            na_string = glue_collapse(which(.x=="NA") , ", ", last = ", and ")
+            s1 = if(sum(.x=="NA", na.rm=TRUE)>1) "s" else ""
+            na_proper = glue_collapse(which(is.na(.x)), ", ", last = ", and ")
+            s2 = if(sum(is.na(.x))>1) "s" else ""
+            warn(c(glue('Cannot describe column "{.y}" as it contains both `NA` (missing values) and "NA" (string)'),
+                  i=glue('NA as strings on row{s1} {na_string}'),
+                  i=glue('NA missing value on row{s2} {na_proper}')))
+            errors[[.y]] = data.frame(name=.y, class="Both `NA` and 'NA'")
+            return(NULL)
         }
         
-        if(is.numeric.and.not.surv(data_x[[.x]]) || is.date(data_x[[.x]])){
-            rtn=cross_numeric(data_x[.x], data_y, funs=funs, funs_arg=funs_arg, 
+        if(inherits(.x, "difftime")){ 
+            lab = get_label(.x)
+            .x = as.numeric(.x) %>% set_label(lab)
+        }
+           
+        data_x[.y] = .x
+        if(is.numeric.and.not.surv(.x) || is.date(.x)){
+            rtn=cross_numeric(data_x[.y], data_y, funs=funs, funs_arg=funs_arg, 
                               showNA=showNA, total=total, label=label, 
                               cor_digits=percent_digits, cor_method=cor_method,
                               test=test, test_args=test_args, effect=effect, effect_args=effect_args)
-        } else if(is.character.or.factor(data_x[[.x]])){
-            rtn=cross_categorical(data_x[.x], data_y, margin=margin,
+        } else if(is.character.or.factor(.x)){
+            rtn=cross_categorical(data_x[.y], data_y, margin=margin,
                                   showNA=showNA, total=total, label=label, percent_digits=percent_digits,
                                   test=test, test_args=test_args, effect=effect, effect_args=effect_args)
-        } else if(is.Surv(data_x[[.x]])){
-            rtn=cross_survival(data_x[.x], data_y, times=times, followup=followup,
+        } else if(is.Surv(.x)){
+            rtn=cross_survival(data_x[.y], data_y, times=times, followup=followup,
                                showNA=showNA, total=total, label=label, surv_digits=percent_digits,
                                test=test, test_args=test_args, effect=effect, effect_args=effect_args)
         } else {
@@ -37,15 +47,15 @@ cross_by = function(data_x, data_y, funs, funs_arg, margin, total, percent_digit
         }
         
         if(is.null(rtn)){
-            errors <<- rbind(errors, data.frame(name=.x, class=paste_classes(data_x[[.x]])))
+            errors[[.y]] = data.frame(name=.y, class=paste_classes(.x))
         }
-        
         rtn
     })
     
+    errors = as.list(errors) %>% map_dfr(identity)
     if(nrow(errors)>0){
         s=if(nrow(errors)>1) "s" else ""
-        errors_s = glue_data(errors, "'{name}' ({class})") %>% glue_collapse(", ", last = " and ")
+        errors_s = glue_data(errors, "'{name}' ({class})") %>% glue_collapse(", ", last = ", and ")
         if(is.null(data_y)){
             warning(call. = FALSE, 
                     glue("Cannot describe column{s} {errors_s}"))
@@ -59,9 +69,10 @@ cross_by = function(data_x, data_y, funs, funs_arg, margin, total, percent_digit
     if("effect" %in% names(rtn_tbl) && any(rtn_tbl$effect=="No effect?")){
         x=rtn_tbl %>% filter(effect=="No effect?") %>% pull(.data$.id) %>% unique
         s=if(length(x)>1) "s" else ""
-        v=glue_collapse(x, "', '", last="' and '")
+        v=glue_collapse(x, "', '", last="', and '")
         warning(glue("Could not calculate crosstable effects for variable{s} '{v}'. Aren't there 2 groups exactly?"), call. = FALSE)
     }
     
+    rownames(rtn_tbl)=NULL
     return(rtn_tbl)
 }
