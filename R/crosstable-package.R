ab9d32e25be17e24c28a382fa96207488b4c4d4c
Dan Chaltiel <dan.chaltiel@gmail.com>
2021-02-01 23:42:20 +0100

    update documentation of options

--- R/crosstable-package.R
+++ R/crosstable-package.R
@@ -17,22 +17,22 @@
 #' Here is a comprehensive list of all options that you can set globally.
 #' 
 #' For flextables:
-#'  + `crosstable_autofit`:
-#'  + `crosstable_compact`:
-#'  + `crosstable_show_test_name`:
-#'  + `crosstable_compact_padding`:
-#'  + `crosstable_wrap_id`: if id contains no spaces, wrap it with this maximum number of characters 
-#'  
-#' For rounding: 
-#'  + `crosstable_only_round`:
+#'  + `crosstable_autofit`, `crosstable_compact`, and `crosstable_show_test_name`: default arguments for [as_flextable()].
+#'  + `crosstable_compact_padding`: left-padding for non-headers rows when `compact=TRUE`.
+#'  + `crosstable_fontsize_body`, `crosstable_fontsize_header`, and `crosstable_fontsize_subheaders`: font sizes for, respectively for normal, header and subheader rows. Subheaders are only considered when `compact=TRUE`.
+#'  + `crosstable_wrap_id`: if `id` contains no spaces, wrap it with this maximum number of characters.
 #'  
 #' For specifying styles in officer docx output: 
-#'  + `crosstable_style_normal`:
-#'  + `crosstable_style_heading`:
-#'  + `crosstable_style_strong`:
-#'  + `crosstable_style_image`:
-#'  + `crosstable_style_legend`:
-#'  + `crosstable_style_list_ordered`:
-#'  + `crosstable_style_list_unordered`:
+#'  + `crosstable_style_normal`
+#'  + `crosstable_style_character`: used in cross-references
+#'  + `crosstable_style_heading`
+#'  + `crosstable_style_strong`
+#'  + `crosstable_style_image`
+#'  + `crosstable_style_legend`
+#'  + `crosstable_style_list_ordered` and `crosstable_style_list_unordered`, mandatory for [body_add_list()] to work.
+#'  
+#' Misc: 
+#'  + `crosstable_only_round`: default argument for [format_fixed()]
+#'  + `crosstable_units`: default units in [body_add_gg2()] and [body_add_img2()]
 #' 
 NULL
