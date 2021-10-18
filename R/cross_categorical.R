74126a8ba2b3cfe49defe39529b95be2e58cb0c4
Dan Chaltiel <dan.chaltiel@gmail.com>
2021-10-18 12:58:16 +0200

    moved summarise_X functions to cross_X files

--- R/cross_categorical.R
+++ R/cross_categorical.R
@@ -36,3 +36,121 @@ cross_categorical=function(data_x, data_y, showNA, total, label, percent_digits,
     rtn
 }
 
+
+#' @importFrom checkmate assert_numeric assert_character
+#' @importFrom glue glue
+#' @importFrom dplyr mutate mutate_at vars select .data
+#' @keywords internal
+#' @noRd
+summarize_categorical_single = function(x, showNA, total, digits, margin){
+    str(x)
+    print(class(x))
+    print(margin)
+    tb=table(x, useNA = "no")
+    print(tb)
+    tbd = as.data.frame(tb, responseName="Freq", stringsAsFactors=FALSE)
+    print(tbd)
+    print(class(tbd))
+    print(dimnames(tbd))
+    print(tbd$x)
+    print(tbd$Freq)
+    select(tbd, variable="x", value="Freq") %>% print()
+    if(identical(margin,-1)){
+        rtn = table(x, useNA = "no") %>% 
+            as.data.frame(responseName="Freq", stringsAsFactors=FALSE) %>% 
+            select(variable="x", value="Freq")
+    } else {
+        rtn = table(x, useNA = "no") %>% 
+            as.data.frame(responseName="Freq", stringsAsFactors=FALSE) %>% 
+            mutate(p=100*.data$Freq/sum(.data$Freq)) %>% 
+            mutate_at(vars("p"), format_fixed, digits=digits) %>% 
+            mutate(value=glue("{Freq} ({p}%)")) %>% 
+            select(variable="x", value="value")
+    }
+    
+    .showNA = showNA=="always" || showNA=="ifany" && (anyNA(x))
+    if(.showNA){
+        rtn = rbind(rtn, data.frame(variable="NA", value=sum(is.na(x))))
+    }
+    
+    if (2 %in% total && identical(margin,-1)) {
+        rtn = rbind(rtn, data.frame(variable="Total", value=sum(table(x, useNA='always'))))
+    } else if (2 %in% total && !identical(margin,-1)) {
+        value = glue("{sum(table(x, useNA='always'))} ({format_fixed(100, digits=digits)}%)")
+        rtn = rbind(rtn, data.frame(variable="Total", value=value))
+    }
+    
+    rtn %>% mutate_all(as.character)
+}
+
+
+
+
+#' @importFrom dplyr mutate mutate_at mutate_all transmute vars starts_with left_join pull .data
+#' @importFrom purrr map reduce    
+#' @importFrom tidyr unite pivot_wider
+#' @importFrom glue glue
+#' @keywords internal
+#' @noRd
+summarize_categorical_by = function(x, by, margin, showNA, total, digits, 
+                                    test, test_args, effect, effect_args){
+    nn = table(x, by, useNA=showNA)
+    .tbl = as.data.frame(nn, responseName="Freq", stringsAsFactors=FALSE)
+    if(identical(margin,-1)){
+        rtn = .tbl %>% 
+            transmute(variable=replace_na(x, "NA"), by=.data$by, Freq=.data$Freq) %>% 
+            pivot_wider(names_from="by", values_from = "Freq")
+    } else {
+        if(length(unique(by))==1) margin=2
+        
+        .ptbl = margin %>% 
+            map(~{
+                if(.x==0)
+                    tmp=table(x, by, useNA="no") %>% {./sum(.)}
+                else
+                    tmp=table(x, by, useNA="no") %>% prop.table(margin=.x)
+                tmp %>% 
+                    as.data.frame(responseName=paste0("p",.x), stringsAsFactors=FALSE) %>%
+                    mutate(across(starts_with("p"), 
+                                  ~format_fixed(100*., digits=digits) %>% paste0("%")))
+            }) %>% 
+            reduce(left_join, by=c("x", "by")) %>% 
+            unite(col="p", starts_with("p"), sep=" / ")
+        rtn = .tbl %>% 
+            left_join(.ptbl, by=c("x", "by")) %>%
+            mutate(value=ifelse(is.na(x)|is.na(by), .data$Freq, glue("{Freq} ({p})"))) %>%
+            transmute(variable=replace_na(x, "NA"), by=.data$by, value=.data$value) %>%
+            pivot_wider(names_from="by", values_from = "value")
+    }
+    
+    
+    if(2 %in% total){
+        mt=margin.table(nn, margin=2) %>% as.numeric()
+        if(identical(margin,-1)){
+            line=mt
+        } else{
+            mt2=margin.table(table(x, by, useNA="no"), margin=2) %>% as.numeric()
+            pct=format_fixed(100*prop.table(mt2), digits) %>% paste0("%")
+            length(pct)=length(mt) #expands with NA
+            line=paste0(mt,ifelse(is.na(pct), "", glue(" ({pct})")))
+        }
+        rtn=rbind(rtn, c("Total", line))
+    }
+    
+    
+    .effect=.test=.total=NULL
+    if(1 %in% total){
+        .total=summarize_categorical_single(x, showNA, total, digits, margin)$value
+    }
+    if(effect) {
+        e = effect_args$effect_tabular(x, by, effect_args$conf_level)
+        .effect = effect_args$effect_display(e, digits = effect_args$digits)
+    }
+    if(test) {
+        .test = test_args$test_display(test_args$test_tabular(x, by), digits = test_args$plim, 
+                                       method = test_args$show_method)
+    }
+    rtn %>%
+        mutate(Total=.total, effect=.effect, test=.test) %>% 
+        mutate_all(as.character)
+}
--- R/cross_numeric.R
+++ R/cross_numeric.R
@@ -41,3 +41,115 @@ cross_numeric = function(data_x, data_y, funs, funs_arg, showNA, total,
     rtn
 }
 
+
+#' Summarize numeric variables
+#' @importFrom checkmate assert_numeric assert_character
+#' @importFrom tibble rownames_to_column
+#' @importFrom dplyr rename mutate_if
+#' @keywords internal
+#' @noRd
+summarize_numeric_single = function(x, funs, funs_arg){
+    imap_dfr(funs, ~{
+        v = do.call(.x, c(list(x), funs_arg))
+        if(length(v)<2){
+            variable = .y
+        } else {
+            variable=names(v)
+            if(.y!=" "){
+                variable = paste(.y, variable)
+            }
+        }
+        if(length(variable)!=length(v)){
+            abort("Summary functions should return a single value", 
+                  class="crosstable_summary_not_scalar")
+        }
+        
+        data.frame(variable=variable, value=v) %>% 
+            mutate_if(is.numeric, format_fixed, !!!funs_arg)
+    })
+}
+
+
+#' Summarize numeric by categorical
+#' @importFrom checkmate assert_numeric assert_character assert_scalar
+#' @importFrom tibble tibble
+#' @importFrom dplyr group_by mutate ungroup mutate_at vars arrange filter .data
+#' @importFrom tidyr nest unnest pivot_wider replace_na
+#' @importFrom purrr map imap reduce
+#' @importFrom forcats fct_explicit_na
+#' @keywords internal
+#' @noRd
+summarize_numeric_factor = function(x, by, funs, funs_arg, showNA, total, 
+                                    cor_digits, cor_method, test, test_args, 
+                                    effect, effect_args){
+    assert_numeric(x)
+    assert_scalar(showNA)
+    .=NULL #mute the R CMD Check note
+    .na=.effect=.test=.total=NULL
+    .showNA = showNA == "always" | (showNA == "ifany" && anyNA(by))
+    
+    if(effect){
+        .effect = effect_args$effect_display(effect_args$effect_summarize(x, by, effect_args$conf_level), 
+                                             digits = cor_digits)
+    }
+    if(test){
+        .test = test_args$test_display(test_args$test_summarize(x, by), digits = test_args$plim, 
+                                       method = test_args$show_method)
+    }
+    if(identical(total, 1) | identical(total, 1:2) | identical(total, TRUE)){
+        .total = summarize_numeric_single(x, funs=funs, funs_arg=funs_arg)[["value"]]
+    }
+    
+    if(.showNA==TRUE) {
+        if(!anyNA(by)) .na="no NA"
+        by_filter=TRUE
+        by = fct_explicit_na(by, "NA")
+    } else{
+        by_filter = !is.na(by) 
+    }
+    
+    by(x[by_filter], by[by_filter], summarize_numeric_single, funs=funs, funs_arg=funs_arg) %>% 
+        imap_dfr(~{
+            if(is.null(.x)) .x=summarize_numeric_single(numeric(0), funs=funs, funs_arg=funs_arg)
+            mutate(.x, by=.y, .before=1)
+        }) %>%
+        pivot_wider(names_from = "by") %>%
+        {if(!is.null(.na)){mutate(.,"NA"=.na)} else .} %>%
+        mutate(Total=.total, effect=.effect, test=.test, 
+               across(everything(), as.character)) %>%
+        ungroup()
+}
+
+
+
+#' Summarize numeric by numeric (correlation)
+#' @importFrom checkmate assert_numeric assert_string assert_count assert_logical assert_list
+#' @importFrom tibble tibble
+#' @importFrom dplyr mutate .data
+#' @importFrom stats cor.test
+#' @importFrom glue glue
+#' @keywords internal
+#' @noRd
+summarize_numeric_numeric = function(x, by, method, digits, test, test_args){
+    assert_numeric(x)
+    assert_numeric(by)
+    assert_string(method)
+    assert_count(digits)
+    assert_logical(test)
+    assert_list(test_args)
+    
+    ct=test_args$test_correlation(x, by, method=method)
+    
+    cor=round(ct$estimate, digits=digits)
+    if(!is.null(ct$conf.int)){
+        ci=round(ct$conf.int, digits=digits)
+        value=glue("{cor} \n95%CI [{ci[1]};{ci[2]}]")
+    } else {
+        value=glue("{cor}")
+    }
+    
+    if(test) .test=test_args$test_display(ct) else .test=NULL
+    
+    tibble(variable=method, value=as.character(value)) %>% mutate(test=.test)
+}
+
