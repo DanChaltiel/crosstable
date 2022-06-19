f87d3a8de9fec8c2c165c240d16097371ae0de68
Dan Chaltiel <dan.chaltiel@gmail.com>
2022-06-19 08:59:39 +0200

    import dplyr, rlang and purrr globally

--- R/crosstable-package.R
+++ R/crosstable-package.R
@@ -2,6 +2,10 @@
 #' The `crosstable`package is centered on the [crosstable()]` function.
 #' See vignettes for more information.
 #' @keywords internal
+#' @import dplyr
+#' @rawNamespace import(purrr, except=c("compact", "as_function", "%@%", "splice"))
+#' @rawNamespace import(rlang, except=c("invoke", "flatten", "flatten_raw", "flatten_dbl", 
+#'                                      "flatten_lgl", "flatten_int", "flatten_chr"))
 #' @name crosstable-package
 #' @aliases Test-package
 "_PACKAGE"
