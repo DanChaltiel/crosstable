3ed2b40e8fd82a153ea4d42df66ac95546a5191a
Dan Chaltiel <dan.chaltiel@gmail.com>
2022-06-28 18:20:10 +0200

    Update crosstable-package.R

--- R/crosstable-package.R
+++ R/crosstable-package.R
@@ -3,6 +3,7 @@
 #' See vignettes for more information.
 #' @keywords internal
 #' @import dplyr
+#' @import stringr
 #' @rawNamespace import(purrr, except=c("compact", "as_function", "%@%", "splice"))
 #' @rawNamespace import(rlang, except=c("invoke", "flatten", "flatten_raw", "flatten_dbl", 
 #'                                      "flatten_lgl", "flatten_int", "flatten_chr"))
