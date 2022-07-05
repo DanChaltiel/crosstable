1f28f1b4236b92b08360a41ebde52bc528a05732
Dan Chaltiel <dan.chaltiel@gmail.com>
2022-07-05 17:28:21 +0200

    enable af(byheader=NA)

--- R/flextable.R
+++ R/flextable.R
@@ -100,6 +100,7 @@ as_flextable.crosstable = function(x, keep_id=FALSE, by_header=NULL,
   has_by =  !is.null(by)
   if(has_by && is.null(by_label)) by_label=by
   if(isTRUE(by_header)) by_header=NULL
+  if(length(by_header)==1 && is.na(by_header)) by_header=FALSE
   if(identical(by_header, "")) by_header=FALSE
 
   generic_labels = get_generic_labels(generic_labels)
