DanChaltiel/crosstable

commit 32bea77ca2d54b504a124ed297b018d2218f487c

simplified by_header handling in af()

R/flextable.R
@@ -112,8 +112,7 @@ as_flextable.crosstable = function(x, keep_id=FALSE, by_header=NULL,
   has_by =  !is.null(by)
   if(has_by && is.null(by_label)) by_label=by
   if(isTRUE(by_header)) by_header=NULL
-  if(length(by_header)==1 && is.na(by_header)) by_header=FALSE
-  if(identical(by_header, "")) by_header=FALSE
+  if(all(is.na(by_header) | by_header=="", na.rm=TRUE)) by_header=FALSE
 
   inner_labels = attr(x, "inner_labels")
   if(!is.null(inner_labels)){

commit 91b7c14a098b0746c39fb7e0ebb840470d11fcef

fixed regression in 32bea77

R/flextable.R
@@ -113,7 +113,7 @@ as_flextable.crosstable = function(x, keep_id=FALSE, by_header=NULL,
   has_by =  !is.null(by)
   if(has_by && is.null(by_label)) by_label=by
   if(isTRUE(by_header)) by_header=NULL
-  if(all(is.na(by_header) | by_header=="", na.rm=TRUE)) by_header=FALSE
+  if(!is.null(by_header) && all(is.na(by_header) | by_header=="", na.rm=TRUE)) by_header=FALSE
 
   inner_labels = attr(x, "inner_labels")
   if(!is.null(inner_labels)){