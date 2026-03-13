84114531f581521a41d28f86780af93c3a27f07e
Dan Chaltiel <15105152+DanChaltiel@users.noreply.github.com>
2026-03-13 20:36:45 +0100

    doc: changed logo
    fixes #115

---
+++ inst/hex.R
@@ -0,0 +1,21 @@
+
+
+hexSticker::sticker(
+  #package name
+  package="crosstable",
+  p_size=18,
+  p_x=1, p_y=0.5,
+  p_color="black",
+  #hexagon
+  h_fill = "white",
+  h_color = "#7fa7c7",
+  h_size = 1.2,
+  #subplot
+  subplot= "inst/figures/table.png",
+  s_x=1, s_y=1.1,
+  s_width=0.8,
+  #output
+  filename="man/figures/logo.png",
+  dpi = 300
+)
+
